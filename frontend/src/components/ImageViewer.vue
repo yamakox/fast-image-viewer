72b8d105da73ab4f88e15d4365452b3c75699f32
yamakox <yamakox@gmail.com>
2026-06-19 18:19:46 +0900

    iPhoneのブラウザでNavbarのオンオフができなくなる不具合を修正した。

--- frontend/src/components/ImageViewer.vue
+++ frontend/src/components/ImageViewer.vue
@@ -166,167 +166,154 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div ref="imageViewerRef" class="image-viewer">
+  <div ref="imageViewerRef" class="image-viewer" @click.stop="navbarVisible = !navbarVisible">
     <!-- 画像 -->
     <img ref="imageRef" :src="imageUrl" :alt="imageName" class="image-content" />
 
-    <div
-      v-if="navbarVisible"
-      class="nav-layer fixed inset-0 z-50 m-0 flex flex-col items-stretch justify-between bg-transparent p-0"
+    <!-- 上部ナビゲーション -->
+    <nav v-if="navbarVisible" class="fixed top-0 left-0 right-0 flex w-full items-center justify-between bg-black/20 p-4">
+      <button
+        class="nav-button text-heading text-center text-sm leading-5 font-medium"
+        @click.stop="toggleImageZoom()"
+      >
+        <svg
+          v-if="!isZoomed"
+          class="h-9 w-9 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke="white"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M8 4H4m0 0v4m0-4 5 5m7-5h4m0 0v4m0-4-5 5M8 20H4m0 0v-4m0 4 5-5m7 5h4m0 0v-4m0 4-5-5"
+          />
+        </svg>
+        <svg
+          v-else
+          class="h-9 w-9 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke="white"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M5 9h4m0 0V5m0 4L4 4m15 5h-4m0 0V5m0 4 5-5M5 15h4m0 0v4m0-4-5 5m15-5h-4m0 0v4m0-4 5 5"
+          />
+        </svg>
+      </button>
+      <div class="truncate text-center leading-5 font-medium text-white">{{ imageName }}</div>
+      <button class="nav-button text-heading text-center text-sm leading-5 font-medium" @click.stop="fireCloseEvent">
+        <svg
+          class="h-9 w-9 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke="white"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M6 18 17.94 6M18 18 6.06 6"
+          />
+        </svg>
+      </button>
+    </nav>
+
+    <!-- 中央ナビゲーション -->
+    <button v-if="navbarVisible"
+      @click.stop="incrementImageIndex(-1)"
+      class="fixed top-1/2 left-0 text-heading bg-black/20 p-4 text-center text-sm leading-5 font-medium"
     >
-      <!-- 上部ナビゲーション -->
-      <nav class="flex w-full items-center justify-between bg-black/20 p-4">
-        <button
-          class="nav-button text-heading text-center text-sm leading-5 font-medium"
-          @click.stop="toggleImageZoom()"
-        >
-          <svg
-            v-if="!isZoomed"
-            class="h-9 w-9 text-gray-800 dark:text-white"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <path
-              stroke="white"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M8 4H4m0 0v4m0-4 5 5m7-5h4m0 0v4m0-4-5 5M8 20H4m0 0v-4m0 4 5-5m7 5h4m0 0v-4m0 4-5-5"
-            />
-          </svg>
-          <svg
-            v-else
-            class="h-9 w-9 text-gray-800 dark:text-white"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <path
-              stroke="white"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M5 9h4m0 0V5m0 4L4 4m15 5h-4m0 0V5m0 4 5-5M5 15h4m0 0v4m0-4-5 5m15-5h-4m0 0v4m0-4 5 5"
-            />
-          </svg>
-        </button>
-        <div class="truncate text-center leading-5 font-medium text-white">{{ imageName }}</div>
-        <button class="nav-button text-heading text-center text-sm leading-5 font-medium" @click.stop="fireCloseEvent">
-          <svg
-            class="h-9 w-9 text-gray-800 dark:text-white"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <path
-              stroke="white"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M6 18 17.94 6M18 18 6.06 6"
-            />
-          </svg>
-        </button>
-      </nav>
+      <svg
+        class="h-9 w-9"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <path stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m15 19-7-7 7-7" />
+      </svg>
+    </button>
+    <button v-if="navbarVisible"
+      @click.stop="incrementImageIndex(1)"
+      class="fixed top-1/2 right-0 text-heading bg-black/20 p-4 text-center text-sm leading-5 font-medium"
+    >
+      <svg
+        class="h-9 w-9 text-gray-800 dark:text-white"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <path stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m9 5 7 7-7 7" />
+      </svg>
+    </button>
 
-      <!-- 中央ナビゲーション -->
-      <nav class="flex w-full items-center justify-between bg-transparent">
-        <button
-          @click.stop="incrementImageIndex(-1)"
-          class="nav-button text-heading bg-black/20 p-4 text-center text-sm leading-5 font-medium"
+    <!-- 下部ナビゲーション -->
+    <nav v-if="navbarVisible"class="fixed bottom-0 left-0 right-0 flex w-full items-center justify-between bg-black/20 p-4">
+      <button
+        @click.stop="toggleFavorite"
+        class="nav-button text-heading ms-auto me-auto text-center text-sm leading-5 font-medium"
+      >
+        <svg
+          v-if="imageFavorite"
+          class="h-9 w-9 text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+          viewBox="0 0 24 24"
         >
-          <svg
-            class="h-9 w-9"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <path stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m15 19-7-7 7-7" />
-          </svg>
-        </button>
-        <button
-          @click.stop="incrementImageIndex(1)"
-          class="nav-button text-heading bg-black/20 p-4 text-center text-sm leading-5 font-medium"
+          <path
+            fill="white"
+            d="m12.75 20.66 6.184-7.098c2.677-2.884 2.559-6.506.754-8.705-.898-1.095-2.206-1.816-3.72-1.855-1.293-.034-2.652.43-3.963 1.442-1.315-1.012-2.678-1.476-3.973-1.442-1.515.04-2.825.76-3.724 1.855-1.806 2.201-1.915 5.823.772 8.706l6.183 7.097c.19.216.46.34.743.34a.985.985 0 0 0 .743-.34Z"
+          />
+        </svg>
+        <svg
+          v-else
+          class="h-9 w-9 text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+          viewBox="0 0 24 24"
         >
-          <svg
-            class="h-9 w-9 text-gray-800 dark:text-white"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <path stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m9 5 7 7-7 7" />
-          </svg>
-        </button>
-      </nav>
-
-      <!-- 下部ナビゲーション -->
-      <nav class="flex w-full items-center justify-between bg-black/20 p-4">
-        <button
-          @click.stop="toggleFavorite"
-          class="nav-button text-heading ms-auto me-auto text-center text-sm leading-5 font-medium"
-        >
-          <svg
-            v-if="imageFavorite"
-            class="h-9 w-9 text-white"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <path
-              fill="white"
-              d="m12.75 20.66 6.184-7.098c2.677-2.884 2.559-6.506.754-8.705-.898-1.095-2.206-1.816-3.72-1.855-1.293-.034-2.652.43-3.963 1.442-1.315-1.012-2.678-1.476-3.973-1.442-1.515.04-2.825.76-3.724 1.855-1.806 2.201-1.915 5.823.772 8.706l6.183 7.097c.19.216.46.34.743.34a.985.985 0 0 0 .743-.34Z"
-            />
-          </svg>
-          <svg
-            v-else
-            class="h-9 w-9 text-white"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <path
-              stroke="white"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M12.01 6.001C6.5 1 1 8 5.782 13.001L12.011 20l6.23-7C23 8 17.5 1 12.01 6.002Z"
-            />
-          </svg>
-        </button>
-      </nav>
-    </div>
+          <path
+            stroke="white"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12.01 6.001C6.5 1 1 8 5.782 13.001L12.011 20l6.23-7C23 8 17.5 1 12.01 6.002Z"
+          />
+        </svg>
+      </button>
+    </nav>
   </div>
 </template>
 
 <style scoped>
-.nav-layer {
-  pointer-events: none;
-}
-.nav-button {
-  pointer-events: auto;
-}
 .image-viewer {
   position: fixed;
   inset: 0;
