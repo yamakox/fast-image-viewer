11507e124514216b44f1e70b381f11e3ecec6090
yamakox <yamakox@gmail.com>
2026-06-22 11:37:14 +0900

    ユーザー登録に成功した場合、メッセージ表示するように改良した。

--- frontend/src/pages/RegisterPage.vue
+++ frontend/src/pages/RegisterPage.vue
@@ -1,22 +1,25 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { useRouter } from 'vue-router'
+import { ref, type Ref } from 'vue'
+// import { useRouter } from 'vue-router'
 import { postData } from '../util'
 
-const router = useRouter()
-const username = ref('')
-const password = ref('')
-const error = ref('')
+// const router = useRouter()
+const username: Ref<string> = ref('')
+const password: Ref<string> = ref('')
+const success: Ref<boolean> = ref(false)
+const error: Ref<string> = ref('')
 
 async function handleSubmit(event: Event) {
   event.preventDefault()
   error.value = ''
+  success.value = false
   const { ok, result } = await postData('/api/v1/register', {
     username: username.value,
     password: password.value,
   })
   if (ok) {
-    router.push('/login')
+    // router.push('/login')
+    success.value = true
     return
   }
   error.value = (result?.detail as string) ?? 'ユーザー登録に失敗しました。'
@@ -25,7 +28,9 @@ async function handleSubmit(event: Event) {
 
 <template>
   <div class="register-page-container">
-    <div class="text-heading mb-5 text-xl">Fast Image Viewer</div>
+    <div class="text-heading mb-5 text-xl">新しいユーザーの追加</div>
+    <p v-if="error" class="mb-5 text-sm text-red-600">{{ error }}</p>
+    <p v-if="success" class="mb-5 text-sm text-green-600">ユーザー登録が完了しました。<a href="/login" class="text-brand hover:text-brand-strong">ログイン</a>してください。</p>
     <form class="mx-auto max-w-sm" @submit="handleSubmit">
       <div class="mb-5">
         <label for="username" class="text-heading mb-2.5 block text-sm font-medium">ユーザー名:</label>
@@ -47,7 +52,6 @@ async function handleSubmit(event: Event) {
           required
         />
       </div>
-      <p v-if="error" class="mb-5 text-sm text-red-600">{{ error }}</p>
       <button
         type="submit"
         class="bg-brand hover:bg-brand-strong focus:ring-brand-medium rounded-base box-border border border-transparent px-4 py-2.5 text-sm leading-5 font-medium text-white shadow-xs focus:ring-4 focus:outline-none"
